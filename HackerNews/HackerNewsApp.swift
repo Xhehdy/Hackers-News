57e5efd417051fb968ffac13340a15a73cebbd0c
Ikechukwu Zion <ikechukwuzion@gmail.com>
2024-04-03 16:36:58 +0100

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Ikechukwu Zion on 03/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
